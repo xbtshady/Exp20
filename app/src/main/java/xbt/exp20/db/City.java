2c2f7fe3c251d6d2db0a643ced060f31d048959f
xbtshady <943217258@qq.com>
2017-09-07 19:58:13 +0800

    Initial commit

---
+++ app/src/main/java/xbt/exp20/db/City.java
@@ -0,0 +1,50 @@
+package xbt.exp20.db;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * Created by xbt on 2017/9/7.
+ */
+
+public class City extends DataSupport {
+
+    private int id;
+
+    private String cityName;//城市的名字
+
+    private int cityCode;//城市的代号
+
+    private int provinceId;//城市所在省的id值
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCityName() {
+        return cityName;
+    }
+
+    public void setCityName(String cityName) {
+        this.cityName = cityName;
+    }
+
+    public int getCityCode() {
+        return cityCode;
+    }
+
+    public void setCityCode(int cityCode) {
+        this.cityCode = cityCode;
+    }
+
+    public int getProvinceId() {
+        return provinceId;
+    }
+
+    public void setProvinceId(int provinceId) {
+        this.provinceId = provinceId;
+    }
+}
